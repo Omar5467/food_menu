8415a76707be072ab83d5c9daeaee1d2fdd3d312
Omar <Omarmoha5467@gmail.com>
2026-04-07 15:21:50 +0300

    PosgreSQL

--- myapp/models.py
+++ myapp/models.py
@@ -6,12 +6,22 @@ from django.contrib.auth.models import User
 
 class item(models.Model):
     def __str__(self):
-        return self.item_name
+        return self.item_name + ' - ' + str(self.item_price)
     
     def get_absolute_url(self):
         return reverse('myapp:index')
     user_name = models.ForeignKey(User,on_delete=models.CASCADE,default=2)
     item_name = models.CharField(max_length=20)
-    item_price = models.FloatField(max_length=10)
+    item_price = models.DecimalField(max_digits=6, decimal_places=2)
     item_desc = models.TextField(max_length=50)
-    item_image = models.CharField(max_length=500,default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPSa35DdV7zBu1GZnX78dUWbnG3dxMo9GfbQ&s')
+    item_image = models.URLField(max_length=500,default='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPSa35DdV7zBu1GZnX78dUWbnG3dxMo9GfbQ&s')
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Category(models.Model):
+    def __str__(self):
+        return self.name
+    
+    name = models.CharField(max_length=100)
+    added_on = models.DateField(auto_now = True)
